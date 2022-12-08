fdd159dd5b2c062c5c95474f1d5c8c941c6bb553
YH-LEE21 <ksmwy9029@naver.com>
2022-12-08 16:27:32 +0900

    divisor,productOfTwoNumbers funtion .py rewrite

--- Level_0/나머지 구하기/solution.py
+++ Level_0/나머지 구하기/solution.py
@@ -1,5 +1,8 @@
 def divisor(num1,num2):
-    return num1%num2
+    if 0<num1<=100 and 0<num2<=100:
+        return num1%num2
+    return "Error!! num1 : 1~100, num2 : 1~100"
 
 print(divisor(3,2))
 print(divisor(10,5))
+print(divisor(0,2))#"Error!! num1 : 1~100, num2 : 1~100"
--- Level_0/두 수의 곱/solution.py
+++ Level_0/두 수의 곱/solution.py
@@ -1,5 +1,7 @@
 def productOfTwoNumbers(num1, num2):
-    return num1*num2
+    if -1<num1<101 and -1<num2<101:
+        return num1*num2
+    return "Error!! num1 : 0~100, num2 : 0~100"
 print(productOfTwoNumbers(3,4))
 print(productOfTwoNumbers(27,19))
 
