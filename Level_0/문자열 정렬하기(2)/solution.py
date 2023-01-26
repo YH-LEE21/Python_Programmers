f30ef1d48d372ae74c83dbf09a44af9ed8edfb32
YH-LEE21 <ksmwy9029@naver.com>
2023-01-26 14:17:47 +0900

    Day 20

---
+++ Level_0/문자열 정렬하기(2)/solution.py
@@ -0,0 +1,6 @@
+def strNumSort2(my_string):
+    return "".join(sorted(my_string.lower()))
+
+print(strNumSort2("Bcad"))
+print(strNumSort2("heLLo"))
+print(strNumSort2("Python"))
---
+++ Level_0/합성수 찾기/solution.py
@@ -0,0 +1,13 @@
+def findCompositeNumbers(n):
+    answer = 0
+    for i in range(1,n+1):
+        count = 0
+        for j in range(1,i+1):
+            if i%j == 0:
+                count += 1
+        if count >= 3:
+            answer += 1
+    return answer
+
+print(findCompositeNumbers(10))
+print(findCompositeNumbers(15))
