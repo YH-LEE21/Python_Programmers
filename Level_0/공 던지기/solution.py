aa3a850306b0a50e23a10707ddf4bcbfa1ff264f
YH-LEE21 <ksmwy9029@naver.com>
2023-02-27 22:55:33 +0900

    Day 33

--- Level_0/공 던지기/solution.py
+++ Level_0/공 던지기/solution.py
@@ -1,3 +1,7 @@
 def ball_throw(numbers, k):
-    answer = 0
-    return answer
+    answer = numbers[0::2] if len(numbers)%2==0 else numbers[0::2]+numbers[1::2]
+    return answer[k%len(answer)-1]
+
+print(ball_throw([1, 2, 3, 4]  ,2))
+print(ball_throw([1, 2, 3, 4, 5, 6]  ,5))
+print(ball_throw([1, 2, 3]  ,3))
