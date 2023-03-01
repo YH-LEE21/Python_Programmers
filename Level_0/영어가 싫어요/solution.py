90bb7b4577e4f705c0a608d26fbea65bdb7faa3b
YH-LEE21 <ksmwy9029@naver.com>
2023-03-01 15:39:24 +0900

    Day 34

--- Level_0/영어가 싫어요/solution.py
+++ Level_0/영어가 싫어요/solution.py
@@ -1,6 +1,11 @@
 def hate_English(numbers):
-    answer = 0
-    return answer
+    dic_Num = {"zero" : 0, "one" : 1, "two" : 2, "three" : 3, "four" : 4, "five" : 5, "six" : 6, "seven" : 7, "eight" : 8, "nine" : 9}
+    for key in dic_Num.keys():
+        numbers = numbers.replace(key,str(dic_Num[key]))
+    return int(numbers)
 
+#123456789
 print(hate_English("onetwothreefourfivesixseveneightnine"))
+
+#14067
 print(hate_English("onefourzerosixseven"))
